5e7e7980a31e67f033bfc1ee382255f03bdf6ae8
maysampx <pasdari.meysam@gmail.com>
2024-09-18 09:43:56 +0200

    Minor updates on annotations.

--- src/main/java/com/streamprocessor/iot/IoTStreamDataProcessorApplication.java
+++ src/main/java/com/streamprocessor/iot/IoTStreamDataProcessorApplication.java
@@ -2,12 +2,10 @@ package com.streamprocessor.iot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-@ComponentScan(basePackages = "com.streamprocessor.iot")
 public class IoTStreamDataProcessorApplication {
 
 	public static void main(String[] args) {
